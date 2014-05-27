e76098594c736491a69a3c1abeef7ea4c677fd79
Duco Dokter <dokter@w20e.com>
2014-05-27 13:56:18 +0200

    Prepare release 1.0.1

--- setup.py
+++ setup.py
@@ -11,18 +11,19 @@ requires = [
     ]
 
 setup(name='djinn_search',
-      version="1.0.0",
+      version="1.0.1",
       description='Djinn Intranet search module',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: Freely Distributable",
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP :: Site Management",
-        "Topic :: Software Development :: Libraries :: Application Frameworks"
-        ],
+          "Development Status :: 3 - Alpha",
+          "Framework :: Django",
+          "Intended Audience :: Developers",
+          "License :: Freely Distributable",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Site Management",
+          "Topic :: Software Development :: Libraries :: "
+          "Application Frameworks"
+      ],
       author='PythonUnited',
       author_email='info@pythonunited.com',
       license='beer-ware',
@@ -31,10 +32,10 @@ setup(name='djinn_search',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires = requires,
-      tests_require= requires,
+      install_requires=requires,
+      tests_require=requires,
       test_suite="djinn_search",
-      entry_points = """\
+      entry_points="""\
       [djinn.app]
       urls=djinn_search:get_urls
       """
